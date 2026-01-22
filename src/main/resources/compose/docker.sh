bd66af6cc97ced3031f262409505d14da0310085
nacycher <nacycher@163.com>
2026-01-22 19:56:22 +0800

    docker模块制作镜像

--- src/main/resources/compose/docker.sh
+++ src/main/resources/compose/docker.sh
@@ -1,7 +1,7 @@
 # 先清理旧容器
-docker stop kafka01 && docker rm kafka01
+docker stop kafka && docker rm kafka
 
-docker run --name kafka01 \
+docker run --name kafka \
 --network netkafka \
 -p 9092:9092 -p 9093:9093 -p 9094:9094 \
 -e KAFKA_CFG_NODE_ID=0 \
@@ -17,13 +17,24 @@ docker run --name kafka01 \
 -v /tmp/kafka-data:/bitnami/kafka/data \
 -d bitnami/kafka:3.6.2
 
+docker run -d \
+  --network  netkafka \
+  --name mongo \
+  -p 172.26.122.77:27017:27017 \
+  -v /data/mongo/db:/data/db \
+  --restart always \
+  -e MONGO_INITDB_ROOT_USERNAME=root \
+  -e MONGO_INITDB_ROOT_PASSWORD=1qaz2WSX \
+  mongo:4.0.5
 
 docker run -d  --name spp-actuator --network netkafka  -p 18093:18093 spp/actuator:20260121v1
 docker run -d  --name spp-ap-worker --network netkafka -p 10006:10006 spp/ap-worker:20260121v1
 docker run -d  --name spp-kafka --network netkafka -p 18086:18086 spp/kafka:20260121v1
-docker run -d  --name spp-admin --network netkafka  -p 10003:10003 spp/spp-admin:20260121v1
+docker run -d  --name spp-admin --network netkafka  -p 10003:10003  spp/spp-admin:20260121v1
 docker run -d  --name spp-xxljob --network netkafka -p 8081:8081 spp/xxl-job-admin:20260121v1
 
+docker exec -it spp-actuator /bin/bash
+docker exec -it spp-ap-worker tail-f /logs/nohup.out
 
 
 linuxscp:
